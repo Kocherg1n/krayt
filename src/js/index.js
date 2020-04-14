5afb22fc664b863dcc1c9a2abff0b4e6cf9f8415
Yuri Kochergin <kocherg1n@icloud.com>
2020-04-14 14:29:12 +0300

    about, features sections ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,22 @@
-// Main js file
-// see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+// hamburger menu close/open
+const hamburgerButtonOpen = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerButtonClose = document.querySelector('.hamburger-menu__close');
+const mobileNavItem = document.querySelectorAll('.hamburger-menu__link');
+
+hamburgerButtonOpen.addEventListener('click', () => {
+  hamburgerMenu.style.display = 'flex';
+  document.body.style.overflow = 'hidden';
+});
+
+hamburgerButtonClose.addEventListener('click', () => {
+  hamburgerMenu.style.display = 'none';
+  document.body.style.overflow = 'visible';
+});
+
+mobileNavItem.forEach(el => {
+  el.addEventListener('click', () => {
+    document.body.style.overflow = 'visible';
+    hamburgerMenu.style.display = 'none';
+  })
+});
