afd2c416295583a48ed02ec18dd802a9f3a1d28b
Yuri Kochergin <kocherg1n@icloud.com>
2020-04-15 11:55:57 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -13,17 +13,8 @@ window.Vue = require('vue');
 Vue.component('slider-projects', require('./components/slider-projects.vue').default);
 
 // Vue init
-import {Carousel3d, Slide} from 'vue-carousel-3d';
-Vue.use(Carousel3d);
 
 const app = new Vue({
   el: '#app',
-  data: {
-    slides: 7
-  },
-  components: {
-    'carousel-3d': Carousel3d.Carousel3d,
-    'slide': Carousel3d.Slide
-  }
 });
 
